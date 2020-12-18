c5a984f2b4c13dd21ec15023c99db97850e60e53
carlos esteban montealegre barahona <esteban9620@gmail.com>
2020-12-18 10:24:28 -0500

    se agrega la funcionalidad de categoria con sus controladores

---
+++ src/main/java/com/vatia/apirest/repository/CategoriaRepository.java
@@ -0,0 +1,20 @@
+package com.vatia.apirest.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.vatia.apirest.model.Categoria;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
+
+	public Categoria findById(int id);
+	
+	@Transactional
+	@Modifying(clearAutomatically=true)
+	@Query(value = "update webapp_tradebalance.dbo.tbl_categorias set nombre =:newName where id =:id ", nativeQuery = true)
+	public void updateCategoriaNative(@Param("newName") String newName, @Param("id") Long id);
+}
