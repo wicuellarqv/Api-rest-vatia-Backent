c4ca5be2a06050d42d0c1968141d0155db9662ee
Luis Fernandez <lfernandez@qvision.com.co>
2021-01-20 09:51:24 -0500

    Consulta detalle modificada

--- src/main/java/com/vatia/apirest/utils/ContratoDetailResponse.java
+++ src/main/java/com/vatia/apirest/utils/ContratoDetailResponse.java
@@ -2,31 +2,23 @@ package com.vatia.apirest.utils;
 
 public class ContratoDetailResponse {
 
-	private String id_contrato;
-	private String codigo_sic;
-	private String negocio_contrato;
-	private String estado;
-	private String comprador;
-	private String vendedor;
-	private String fecha_inicio;
-	private String fecha_fin;
-	private String condicion;
-	private String formula;
-	private String num_contrato;
-
+	private String id_contrato, num_contrato, codigo_sic,
+	negocio_contrato, comprador, vendedor, estado, fecha_inicio,
+	fecha_fin, condicion, formula;
+	
 	public ContratoDetailResponse(String[] args) {
 		super();
 		this.id_contrato = args[0].trim();
-		this.codigo_sic = args[1].trim();
-		this.negocio_contrato = args[2].trim();
-		this.estado = args[3].trim();
+		this.num_contrato = args[1].trim();
+		this.codigo_sic = args[2].trim();
+		this.negocio_contrato = args[3].trim();
 		this.comprador = args[4].trim();
 		this.vendedor = args[5].trim();
-		this.fecha_inicio = args[6].trim();
-		this.fecha_fin = args[7].trim();
-		this.condicion = args[7].trim();
-		this.formula = args[8].trim();
-		this.num_contrato = args[9].trim();
+		this.estado = args[6].trim();
+		this.fecha_inicio = args[7].trim();
+		this.fecha_fin = args[8].trim();
+		this.condicion = args[9].trim();
+		this.formula= args[10].trim();
 	}
 
 	public String getEstado() {
@@ -35,10 +27,10 @@ public class ContratoDetailResponse {
 
 	@Override
 	public String toString() {
-		return "{'id_contrato'='" + id_contrato + "', 'codigo_sic'='" + codigo_sic + "', 'negocio_contrato'='" + negocio_contrato
-				+ "', 'estado'='" + estado + "', 'comprador'='" + comprador + "', 'vendedor'='" + vendedor + "', 'fecha_inicio'='"
-				+ fecha_inicio + "', 'fecha_fin'='" + fecha_fin + "', 'condicion'='" + condicion + "', 'formula'='" + formula
-				+ "', 'num_contrato'='" + num_contrato + "'}";
+		return "{id_contrato=" + id_contrato + ", codigo_sic=" + codigo_sic + ", negocio_contrato=" + negocio_contrato
+				+ ", estado=" + estado + ", comprador=" + comprador + ", vendedor=" + vendedor + ", fecha_inicio="
+				+ fecha_inicio + ", fecha_fin=" + fecha_fin + ", condicion=" + condicion + ", formula=" + formula
+				+ ", num_contrato=" + num_contrato + "}";
 	}
 
 	public void setEstado(String estado) {
