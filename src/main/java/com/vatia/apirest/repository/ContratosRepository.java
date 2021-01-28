6f55f92fe9ac443a55c2ab2650a59953083fcba9
Williams Cuellar <wcuellar@qvision.com.co>
2021-01-28 08:18:42 -0500

    Ajuste codigo num contrato

--- src/main/java/com/vatia/apirest/repository/ContratosRepository.java
+++ src/main/java/com/vatia/apirest/repository/ContratosRepository.java
@@ -24,7 +24,7 @@ public interface ContratosRepository extends JpaRepository<Contratos, Integer>{
 	@Query(value = "DELETE FROM tbl_contratos WHERE num_id_contrato = ?1", nativeQuery = true)
 	int deleteId(@Param(value = "id") Integer id);	
 	
-	@Query(value = "SELECT num_id_contrato FROM tbl_contratos WHERE num_id_contrato = ?1", nativeQuery = true)
+	@Query(value = "SELECT  TOP 1  num_contrato FROM tbl_contratos WHERE num_contrato = ?1", nativeQuery = true)
 	String idContrato (Integer id);
 	
 	@Query(value = "SELECT TOP 1 str_codigo_sic_contrato FROM tbl_contratos WHERE str_codigo_sic_contrato = ?1", nativeQuery = true)
