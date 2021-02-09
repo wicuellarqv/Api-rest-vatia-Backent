973458b58e6b85765abf9fddedd6cb34071db937
carlos esteban montealegre barahona <esteban9620@gmail.com>
2021-02-09 17:19:13 -0500

    ajustes de querys para clientes

--- src/main/java/com/vatia/apirest/repositoryBI/ClienteRepository.java
+++ src/main/java/com/vatia/apirest/repositoryBI/ClienteRepository.java
@@ -16,35 +16,36 @@ public interface ClienteRepository extends PagingAndSortingRepository<Cliente, C
 	public List<Cliente> findByNombreFacturacionLikeOrderByNombreFacturacionAsc(String nombreLike);
 
 	@Query(value = "SELECT ID_CLIENTE,NOMBRE_FACTURACION,FECHA_ESTADO_HIS_MER"
-			+ "  FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE " + "  where NOMBRE_FACTURACION like :nombre "
+			+ "  FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE where NOMBRE_FACTURACION like :nombre AND FECHA_ESTADO_HIS_MER IS NOT NULL "
 			+ "  group by ID_CLIENTE,NOMBRE_FACTURACION,FECHA_ESTADO_HIS_MER order by NOMBRE_FACTURACION ASC", nativeQuery = true)
 	public List<Object> nativeFindByLikeClienteNombreFacturacion(@Param("nombre") String nombreLike);
 
 	@Query(value = "SELECT ID_CLIENTE,NOMBRE_FACTURACION,FECHA_ESTADO_HIS_MER  "
 			+ "			  FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE   "
-			+ "			  where NOMBRE_RAZON_SOCIAL like :razon   "
+			+ "			  where NOMBRE_RAZON_SOCIAL like :razon AND FECHA_ESTADO_HIS_MER IS NOT NULL "
 			+ "			  group by ID_CLIENTE,NOMBRE_FACTURACION,FECHA_ESTADO_HIS_MER order by NOMBRE_FACTURACION ASC", nativeQuery = true)
 	public List<Object> nativeFindByLikeClienteRazonSocial(@Param("razon") String razon);
 
 	@Query(value = "SELECT DISTINCT CATEGORIA_CLIENTE  "
-			+ "			  FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE WHERE CATEGORIA_CLIENTE IS NOT NULL", nativeQuery = true)
+			+ "			  FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE WHERE "
+			+ "CATEGORIA_CLIENTE IS NOT NULL AND FECHA_ESTADO_HIS_MER IS NOT NULL", nativeQuery = true)
 	public List<Object> nativeCategoriasCliente();
 
 	public List<Cliente> findByIdCliente(String id);
 
 	@Query(value = "SELECT * FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE   "
-			+ "			  where NOMBRE_RAZON_SOCIAL like :razon   "
+			+ "			  where NOMBRE_RAZON_SOCIAL like :razon AND CATEGORIA_CLIENTE IS NOT NULL AND FECHA_ESTADO_HIS_MER IS NOT NULL "
 			+ "			  order by NOMBRE_FACTURACION ASC", nativeQuery = true)
 	public Page<Cliente> nativeFindByLikeClienteRazonPage(Pageable page, @Param("razon") String razon);
 
 	@Query(value = "SELECT * FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE   "
-			+ "			  where CATEGORIA_CLIENTE like :categoria   "
+			+ "			  where CATEGORIA_CLIENTE = :categoria AND FECHA_ESTADO_HIS_MER IS NOT NULL "
 			+ "			  order by NOMBRE_FACTURACION ASC", nativeQuery = true)
 	public Page<Cliente> nativeFindByLikeClienteCatPage(Pageable page, @Param("categoria") String categoria);
 
 	@Query(value = "SELECT * FROM STG_TRADEBALANCE.dbo.STG_DIM_CLIENTE   "
-			+ "			  where CATEGORIA_CLIENTE like :categoria and   "
-			+ "			  NOMBRE_RAZON_SOCIAL like :razon   "
+			+ "			  where CATEGORIA_CLIENTE = :categoria and   "
+			+ "			  NOMBRE_RAZON_SOCIAL like :razon  AND CATEGORIA_CLIENTE IS NOT NULL  AND FECHA_ESTADO_HIS_MER IS NOT NULL "
 			+ "			  order by NOMBRE_FACTURACION ASC", nativeQuery = true)
 	public Page<Cliente> nativeFindByLikeClienteCatAndRazPage(Pageable page, @Param("categoria") String categoria,
 			@Param("razon") String razon);
